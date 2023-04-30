59d1ebca8b215364639441c872e02ded0d380213
Idris <idris.taiwo@me.com>
2023-04-30 12:09:06 +0100

    initial commit

---
+++ src/pages/basic.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const basic = () => {
+  return <div>basic</div>;
+};
+
+export default basic;
---
+++ src/pages/recipes.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import { Text } from "idvor-components";
+
+const RecipePage: NextPage = () => {
+  return <div>hello </div>;
+};
+
+export default RecipePage;
